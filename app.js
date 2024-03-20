420990996c8a042f3b44dfccaa78a3b260bc1e0d
DESKTOP-J4KHL62\ryu45 <ryu456321@naver.com>
2024-03-20 19:51:22 +0900

    최종

---
+++ app.js
@@ -0,0 +1,35 @@
+let age = 32;
+let greetingText = "HM";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+let totalAdultYears;
+
+function calculateAdultYears(userAge) {
+  let result;
+  result = userAge - 18;
+  return result;
+}
+
+totalAdultYears = calculateAdultYears(age);
+console.log(totalAdultYears);
+
+age = 45;
+totalAdultYears = calculateAdultYears(age);
+
+console.log(totalAdultYears);
+
+let person = {
+  name: "Max", // Property
+  greet() {    // Method
+    console.log("Hello!");
+  },
+};
+
+person.greet();
+
+
---
+++ operation.js
@@ -0,0 +1,21 @@
+// Math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4); // 10 / 4 => 2 * 4 => Reainder: 2
+console.log(10 % 3); // 10 / 3 => 3 * 3 => Expected Reainder: 1
+
+console.log((10 + 3 - 5) * 10);
+
+console.log("Ryu" + " " + "Hunmin");
+console.log("Ryu" - "yu");
+
+let userName = "Ryu";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ["Sports", "Cooking"];
+console.log(hobbies.length); // 요소의 개수
+
